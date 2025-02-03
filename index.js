2260c8c95888a6ddeeeb55ca76634655895612b2
Dvdrepetto <davidmrepetto@gmail.com>
2025-02-03 19:00:23 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor(items, length) {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    for(let i = 0; i < this.length - 1; i++){
+      if(pos < 0 || pos >= this.length){
+        throw new Error("OutOfBounds");
+      }
+    }
+    return  this.items[pos];
+  }
 
-  max() {}
+  max() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return Math.max(...this.items)
+  }
 
-  min() {}
+  min() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items)
+  }
 
-  sum() {}
+  sum() {
+    if (this.items.length === 0) return 0;
+    return this.items.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    const sum = this.items.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+    return sum / this.items.length;
+  }
 }
-
 module.exports = SortedList;
